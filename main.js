eb4d95e511176d50f87d8dc9994733d1cf0b0e50
John Stilwell <johnrstilwell@codeimmersives.com>
2019-08-19 11:19:52 -0400

    canvas working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // const palate = document.querySelector('.palette');
 const paintbrush = document.querySelector('.current-brush');
-const canvas = document.querySelector('.square');
+const canvasSquare = document.querySelectorAll('.square');
 const palettes = document.querySelectorAll('.palette-color');
 
 palettes.forEach(function(palette) {
@@ -10,9 +10,16 @@ palettes.forEach(function(palette) {
 });
 
 
-palettes.forEach(function (palette) {
-    palette.addEventListener('click', function (event) {
-        paintbrush.classList.replace(paintbrush.classList[1], event.target.classList[1]);
+// palettes.forEach(function (palette) {
+//     palette.addEventListener('click', function (event) {
+//         paintbrush.classList.replace(paintbrush.classList[1], event.target.classList[1]);
+//     });
+// });
+
+
+canvasSquare.forEach(function (canvas) {
+    canvas.addEventListener('click', function (event) {
+        canvas.classList.replace(canvas.classList[1], paintbrush.classList[1]);
     });
 });
 
@@ -20,10 +27,10 @@ palettes.forEach(function (palette) {
 // selects the colors next to the palette
 //   for each of palette items add an event listener of click and run FN brushChange
 // function paletteChanges () {
-    //     for(let i = 0; i < paletteChange.length; i++) {
-        // console.log('paletteChange');
+//         for(let i = 0; i < paletteChange.length; i++) {
+//         console.log('paletteChange');
 
-//selects the 9 square canvas
+// selects the 9 square canvas
 // const canvasChange = document.querySelectorAll(`.square`);
 // // for each item in the canvas add an EL of click and run FN canvasColor
 // canvasChange.addEventListener(`click`, function () {
